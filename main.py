866f673f1f462a7b03fe189fd8d1e9f2bdeb826f
Mana <57663038+Manaball123@users.noreply.github.com>
2024-07-19 13:11:46 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import cv2
+import os
+import numpy as np 
+import dhash
+from PIL import Image
+
+def mkdir_if_not_exist(dir : str):
+    if not os.path.isdir(dir):
+        os.makedirs(dir)
+
+
+
+global_redundancy_set = set()
+def is_frame_redundant(image) -> bool:
+    row, col = dhash.dhash_row_col(image)
+    hashed = dhash.format_hex(row, col)
+    if hashed in global_redundancy_set:
+        return True
+    global_redundancy_set.add(hashed)
+    return False
+
+
+vid_path = "./Reading the White Album 2 Manga Online Vol.3 [Part 5] [Oms2iOJfgXY].mkv"
+out_path = "frames"
+
+
+def extract_images(path_in, path_out):
+    count = 0
+    vidcap = cv2.VideoCapture(path_in)
+    success, image = vidcap.read()
+    success = True
+    while success:
+        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*1000))    # added this line 
+        success,image = vidcap.read()
+        image = Image.fromarray(image)
+        print ('Read a new frame: ', success)
+
+        if is_frame_redundant(image):
+            print("Frame redundant. Skipping.")
+        else:
+            
+            image.save(f"{path_out}/frame{count}.jpg")     # save frame as JPEG file
+        count += 1
+
+
+def main():
+    mkdir_if_not_exist(out_path)
+    extract_images(vid_path, out_path)
+
+
+main()
